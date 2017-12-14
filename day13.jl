3d7dab1aba0ff42b3f706a06c18e33bb5aaba8e9
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2017-12-14 15:14:48 -0500

    trying hard to re-use (nearly) exact day 12 code

--- day13.jl
+++ day13.jl
@@ -17,3 +17,47 @@ end
 
 # part 2: count number of connected components. can re-use paint with a type
 # where getindex is overloaded?
+
+grid = Array{Char}(128, 128)
+for row in 1:128
+    row_hash = knot_hash("$key-$(row-1)")
+    grid[row, :] = [c for c in bits(eval(parse("0x$(row_hash)")))]
+end
+
+struct GridEdges{T}
+    grid::Matrix{T}
+    occupied::T
+end
+
+# get edges
+function Base.getindex(g::GridEdges, i::Int...)
+    [is for is in neighbors(i) if (checkbounds(Bool, g.grid, is...) && g.grid[is...] == g.occupied)]
+end
+
+neighbors(i::Tuple{Int,Int}) = ((i[1]+1, i[2]), (i[1]-1, i[2]), (i[1], i[2]+1), (i[1], i[2]-1))
+
+
+## copied from day 12
+function paint(i, color, edges, components)
+    if components[i...] == 0
+        components[i...] = color
+        for j in edges[i...]
+            components = paint(j, color, edges, components)
+        end
+    end
+    return components
+end
+
+components = zeros(Int, size(grid))
+components[grid.=='0'] = -1
+
+
+ge = GridEdges(grid, '1')
+comp_i = 0
+while findfirst(components, 0) > 0
+    @show comp_i += 1
+    components = paint(ind2sub(components, findfirst(components, 0)),
+                       comp_i,
+                       ge,
+                       components)
+end
