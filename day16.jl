26630dd2dbd77dd6541a1a4ab59f36d96e2f8925
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2017-12-17 12:33:58 -0500

    cleanup

--- day16.jl
+++ day16.jl
@@ -13,84 +13,12 @@ using ArgCheck
 # dict for reverse index, and un-shift numeric indices for 'x'.
 
 
-mutable struct Programs
-    programs::Vector{Char}
-    shift::Int
-    n::Int
-    revindex::Vector{Int}
-    Programs(ps::Vector{Char}) =
-        new(ps, 0, length(ps), collect(1:length(ps)))
-end
-
-Base.show(io::IO, p::Programs) =
-    println(io, "Programs: $(String(circshift(p.programs, p.shift)))")
-
-
-s(p::Programs, n::Int) = (p.shift = (p.shift+n)%p.n; p)
-
-function x(p::Programs, a::Int, b::Int)
-    a, b = mod.((a,b) .- p.shift, p.n) .+ 1
-    p.programs[a], p.programs[b] = p.programs[b], p.programs[a]
-    p.revindex[p.programs[a]-'`'] = a
-    p.revindex[p.programs[b]-'`'] = b
-    return p
-end
-
-function p(p::Programs, a::Int, b::Int)
-    ai = p.revindex[a]
-    bi = p.revindex[b]
-    p.programs[ai], p.programs[bi] = b+'a'-1, a+'a'-1
-    p.revindex[a] = bi
-    p.revindex[b] = ai
-    return p
-end
-
-function parse_inst(inst::AbstractString)
-    op = eval(Symbol(inst[1]))
-    args = (split(inst[2:end], '/')...)
-    if op == p
-        args = getindex.(args, 1) .- 'a' .+ 1
-    else
-        args = parse.(Int, args)
-    end
-    return op, args
-end
-
-
-function dance!(ps::Programs, instructions::Vector)
-    for (op, args) in instructions
-        op(ps, args...)
-    end
-    ps
-end
-
-test_str = "s1,x3/4,pe/b" 
-instructions_test = parse_inst.(split(test_str, ","))
-x_test = Programs(collect('a':'e'))
-dance!(x_test, instructions_test)
-
-
-ps = Programs(collect('a':'p'))
-instructions = parse_inst.(split(chomp(readstring("day16.input")), ","))
-dance!(ps, instructions)
-
-using BenchmarkTools
-@benchmark dance!($ps, $instructions)
-
-@profile foreach((i)->dance!(ps, instructions), 1:1_000)
-
-ps = Programs(collect('a':'p'))
-for i in 1:1_000_000_000
-    dance!(ps, instructions)
-    i % 1_000_000 == 0 && println(i)
-end
-
-
-# Okay, this isn't going to work.  but easier: don't need to actually _do_ all
-# the instructions.  keep track of the numerical indices and the mapping from
-# indices to names.  The character thing is a red herring.  Actually, I think we
-# need the _reverse_ names index (char-->index), because we need to do things
-# like "swap the things called 'e' and 'b'", which requires finding 'e' and 'b'.
+# Okay, brute force isn't going to work.  but easier: don't need to actually
+# _do_ all the instructions.  keep track of the numerical indices and the
+# mapping from indices to names.  The character thing is a red herring.
+# Actually, I think we need the _reverse_ names index (char-->index), because we
+# need to do things like "swap the things called 'e' and 'b'", which requires
+# finding 'e' and 'b'.
 
 
 mutable struct Instructions
