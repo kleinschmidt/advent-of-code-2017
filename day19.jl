c58eedef80c25e13e302502860d25ccd5e244eb6
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2017-12-19 17:51:53 -0500

    day 19 star 1

--- day19.jl
+++ day19.jl
@@ -14,60 +14,49 @@ test_input = """
 # to show 
 parse_input(lines::Vector) = reduce(hcat, [c for c in line] for line in lines)
 
-# path = parse_input(split(chomp(test_input), '\n'))
-
-mutable struct Path
-    path::Matrix{Char}
-    letters::Vector{Char}
-    Path(p::Matrix{Char}) = new(p, Char[])
-end
-
-Path(s::S) where S<:AbstractString = Path(parse_input(split(chomp(s), '\n')))
-
-import Base: start, done, next, iteratorsize, show
-
-function show(io::IO, p::Path)
-    println(io, "Path:")
-    for j in 1:size(p.path, 2)
-        for i in 1:size(p.path, 1)
-            print(io, p.path[i,j])
-        end
-        println(io)
-    end
-end
-
-iteratorsize(::Path) = Base.SizeUnknown()
-start(p::Path) = (ind2sub(p.path, findfirst(p.path, '|')), (0,1))
-
-matches(heading::Tuple{Int,Int}, c::Char) = c != ' ' && heading[1] != 0 ? c!='|' : c!='-'
-
 headings = [(0,1), (1,0), (0,-1), (-1,0)]
-neighbors(pos::Tuple{Int,Int}, heading::Tuple{Int,Int}) =
-    [(pos .+ h, h) for h in headings if h != heading .* -1]
-function turn(p::Path, pos::Tuple{Int,Int}, heading::Tuple{Int,Int})
-    for (nex, head) in neighbors(pos, heading)
-        if checkbounds(Bool, p.path, nex...) && matches(head, p.path[nex...])
-            return nex, head
+
+function turn(path::Matrix{Char}, pos, heading)
+    for h in filter(h -> h != heading .* -1, headings)
+        next = pos .+ h
+        if checkbounds(Bool, path, next...) &&
+            path[next...] != ' ' &&
+            path[next...] != (h[1]==0 ? '-' : '|')
+            return (next, h)
         end
     end
-    return ()
+    return (), ()
 end
 
-done(p::Path, state) = isempty(turn(p, state...))
-function next(p::Path, state)
-    c = p.path[state[1]...]
-    c, (c == '+' ? turn(p, state...) : (state[1] .+ state[2], state[2]))
+function foo(path::Matrix{Char})
+    chars = Char[]
+    pos = ind2sub(path, findfirst(path, '|'))
+    heading = (0,1)
+    c = path[pos...]
+    while true
+        # @show c, pos, heading
+        if c == '+'
+            # turn
+            # ...not backwards
+            pos, heading = turn(path, pos, heading)
+            if isempty(pos)
+                return chars
+            end
+            c = path[pos...]
+        else
+            c != '-' && c != '|' && push!(chars, c)
+            pos = pos .+ heading
+            if !checkbounds(Bool, path, pos...) || path[pos...] == ' '
+                return chars
+            end
+            c = path[pos...]
+        end
+    end
 end
 
-Base.length(p::Path) = sum(c != ' ' for c in p.path)
-Base.isempty(p::Path) = length(p) === 0
+path = parse_input(split(chomp(test_input), '\n'))
+permutedims(path, (2,1))
 
-p = Path(test_input)
+foo(path)
 
-chs = Char[]
-for c in p
-    push!(chs, c)
-end
-
-collect(take((c for c in p), 10))
-
+String(parse_input(readlines("day19.input")) |> foo)
